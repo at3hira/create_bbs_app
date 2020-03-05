1a567162bc7c0ba72d89b3d7f0fb072cdc6c6583
hira <hira2633@gmail.com>
2020-03-05 16:59:07 +0900

    create comments store function

--- server/app/Http/Controllers/CommentsController.php
+++ server/app/Http/Controllers/CommentsController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Thread;
 
 class CommentsController extends Controller
 {
-    //
+	public function store(Request $request) 
+	{
+		$params = $request->validate([
+			'thread_id' => 'required| exists:threads,id',
+			'body' => 'required|max:2000',
+		]);
+	//	$params['body'] = htmlspecialchars($params['body']);
+		$thread = Thread::findOrFail($params['thread_id']);
+
+		$thread->comment()->create($params);
+
+		return redirect()->route('threads.show', ['thread' => $thread]);
+	}	
 }
