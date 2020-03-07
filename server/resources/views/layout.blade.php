0c24692205b32feaf13521eeadfbf3c5d6d7e310
hira <hira2633@gmail.com>
2020-03-07 11:20:11 +0900

    add meta viewport

--- server/resources/views/layout.blade.php
+++ server/resources/views/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <title>Laravel BBS</title>
@@ -10,6 +10,7 @@
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous"
 	>--}}
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/site.css') }}" rel="stylesheet">
 </head>
