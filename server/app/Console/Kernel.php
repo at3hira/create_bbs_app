9b576a74c98528b291048aa2739b19add23c43d1
hira <hira2633@gmail.com>
2020-06-05 18:13:49 +0900

    change Task run time

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('command:scrapecommand')
-                  ->hourly();
+                  ->hourlyAt(9);
     }
 
     /**
