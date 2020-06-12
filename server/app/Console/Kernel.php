eb2bf153ecb6265fda790b86c969b66e49c58d79
hira <hira2633@gmail.com>
2020-06-12 16:19:34 +0900

    Change cron execution time

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('command:scrapecommand')
-                  ->hourlyAt(9);
+                  ->hourlyAt(10);
     }
 
     /**
