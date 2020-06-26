7d2bc8538c7f824c6c7ac40edd2c00ac60d9ef8f
hira <hira2633@gmail.com>
2020-06-26 16:36:22 +0900

    Change Site design 　・スレッド一覧最新3件の表示 　・タブレットのグリッドレイアウト 　・サムネイル表示サイズ

--- server/resources/views/threads/index.blade.php
+++ server/resources/views/threads/index.blade.php
@@ -11,7 +11,7 @@
 		
 	<article class="container mt-4">
 		@foreach ($threads as $thread)
-			@if($loop->index <= 2 && $device && $threads->currentPage() === 1)
+{{--			@if($loop->index <= 2 && $device && $threads->currentPage() === 1)
 			<section class="card mb-4 latest-items">
 					<a class="card-link" href="{{ route('threads.show', ['thread' => $thread]) }}">
 						<div class="latest-thumbnail">
@@ -22,36 +22,31 @@
 							<div class="card-meta">
 								<i class="far fa-clock"></i> {{ $thread->created_at->format('Y/m/d') }}
 							</div>	
-							{{--@if ($device)
-								<p class="card-text">
-									{!! nl2br(e(str_limit($thread->body, 60))) !!}
-								</p>
-							@endif
-							--}}
 						</div>
 					</a>	
 				</section>	
 			@else
-				<section class="card mb-4">
-					<a class="card-link" href="{{ route('threads.show', ['thread' => $thread]) }}">
-						<div class="thr_thumbnail">
-							<img src="{{ \Config::get('app.imagePATH') }}/{{ $thread->img_url }}">
-						</div>
-						<div class="card-body">
-							<h2 class="thread_title">{{ str_limit($thread->title, 60) }}</h2>
-							<div class="card-meta">
-								<i class="far fa-clock"></i> {{ $thread->created_at->format('Y/m/d') }}
-							</div>	
-							{{--@if ($device)
-								<p class="card-text">
-									{!! nl2br(e(str_limit($thread->body, 60))) !!}
-								</p>
-							@endif
-							--}}
-						</div>
-					</a>	
-				</section>			
-			@endif
+--}}
+			<section class="card mb-4">
+				<a class="card-link" href="{{ route('threads.show', ['thread' => $thread]) }}">
+					<div class="thr_thumbnail">
+						<img src="{{ \Config::get('app.imagePATH') }}/{{ $thread->img_url }}">
+					</div>
+					<div class="card-body">
+						<h2 class="thread_title">{{ str_limit($thread->title, 60) }}</h2>
+						<div class="card-meta">
+							<i class="far fa-clock"></i> <span class="meta-date">{{ $thread->created_at->format('Y/m/d') }}</span>
+						</div>	
+						{{--@if ($device)
+							<p class="card-text">
+								{!! nl2br(e(str_limit($thread->body, 60))) !!}
+							</p>
+						@endif
+						--}}
+					</div>
+				</a>	
+			</section>			
+{{--			@endif--}}
 		@endforeach
 		<div class="pagination">
 			{{ $threads->links() }}	
