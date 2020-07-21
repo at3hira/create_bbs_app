082b1602bb803700800d37316116a133790af518
hira <hira2633@gmail.com>
2020-07-21 11:45:31 +0900

    Delete unused vue files

--- server/resources/js/app.js
+++ server/resources/js/app.js
@@ -21,15 +21,15 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+// 画像プレビュー機能
 Vue.component('image-data', require('./components/ImageData.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
 const app = new Vue({
     el: '#app',
 });
